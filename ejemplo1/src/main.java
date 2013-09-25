31c6928e040dbf4b4e7b40c670d111fc2e38c812
David Rubio <davidrubiop@gmail.com>
2013-09-25 08:35:46 -0500

    J Option

--- ejemplo1/src/main.java
+++ ejemplo1/src/main.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 
 public class main {
 
@@ -7,6 +9,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+JOptionPane.showMessageDialog(null, "Madrid");
 	}
 
 }
